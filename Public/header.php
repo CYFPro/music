1e41dce7d7c5e74fd7eeaa969cb792f3e0711ca2
CYFPro <41472804+CYFPro@users.noreply.github.com>
2019-05-02 16:09:26 +0800

    Update header.php

--- Public/header.php
+++ Public/header.php
@@ -5,15 +5,8 @@
 <a class="am-topbar-btn am-topbar-toggle am-btn am-btn-sm am-btn-success am-show-sm-only" data-am-collapse="{target: '#doc-topbar-collapse'}"><span class="am-sr-only">导航切换</span> <span class="am-icon-bars"></span></a>
 <div class="am-collapse am-topbar-collapse" id="doc-topbar-collapse">
 <ul class="am-nav am-nav-pills am-topbar-nav">
-<li<?php if ($title=="在线工具") echo ' class="am-active"';?>><a href="/">首页</a></li>
-<li<?php if ($title=="音乐解析") echo ' class="am-active"';?>><a href="/music">音乐</a></li>
-<li<?php if ($title=="视频解析") echo ' class="am-active"';?>><a href="/video">视频</a></li>
-<li<?php if ($title=="全能图床") echo ' class="am-active"';?>><a href="/tuchuang">图床</a></li>
-<li><a href="https://www.liumingye.cn/">博客</a></li>
 </ul>
 <div class="am-topbar-right">
-<a class="am-btn am-btn-primary am-topbar-btn am-btn-sm" data-am-modal="{target: '#fenxiang'}">分享</a>
-<a class="am-btn am-btn-primary am-topbar-btn am-btn-sm" data-am-modal="{target: '#zanzhu'}">赞助</a>
 </div>
 </div>
 </div>
